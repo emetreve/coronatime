2552f8fe7f256ddc75cac228b6aca812cc9d9921
emetreve <emetreve@gmail.com>
2023-04-11 22:37:31 +0400

    wip: implement checkbox

--- lang/en/login.php
+++ lang/en/login.php
@@ -3,4 +3,6 @@
 return [
 	'welcome'             => 'Welcome back',
 	'welcome_full'        => 'Welcome back! Please enter your details',
+	'remember_device'     => 'Remember this device',
+	'forgot_password'     => 'Forgot password?',
 ];
