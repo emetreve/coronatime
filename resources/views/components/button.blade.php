520fd64e897de51249f42af6ad24f703be981211
emetreve <emetreve@gmail.com>
2023-04-13 18:15:13 +0400

    style: add signup-success notification page

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
 @props(['text'])
-<button class="mt-6 rounded-lg text-white bg-green-500 uppercase font-bold text-sm py-4 w-full hover:bg-green-400">
+<button class="mt-6 rounded-lg text-white bg-buttongreen uppercase font-bold text-sm py-4 w-full hover:bg-green-500">
     {{ $text }}
 </button>
