750f35f3e7a59165b7eddc4f14ebe2318528bf55
emetreve <emetreve@gmail.com>
2023-04-13 19:22:10 +0400

    style: edit email subject, line, action

--- lang/en/email-verification.php
+++ lang/en/email-verification.php
@@ -3,4 +3,7 @@
 return [
 	'sent_email'       => 'We have sent you a confirmation email',
 	'account_confirmed'=> 'Your account is confirmed, you can sign in',
+	'email_subject'    => 'Verify Email Address',
+	'email_line'       => 'Click the button below to verify your email address.',
+	'email_action'     => 'Verify Email Address',
 ];
