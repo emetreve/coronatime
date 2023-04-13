4077095790d85753d3e08186052ca624a346b92a
emetreve <emetreve@gmail.com>
2023-04-13 18:16:17 +0400

    feat: add link to log in button

--- resources/views/auth/signup-success.blade.php
+++ resources/views/auth/signup-success.blade.php
@@ -5,7 +5,9 @@
             <img class="h-20 pt-6 lg:h-24" src="{{ asset('checkmark.gif') }}" />
             <p class="text-gray-600 mt-4 lg:text-2xl">{{ __('email-verification.account_confirmed') }}</p>
             <div class="w-full px-4 absolute bottom-5 lg:w-1/4 lg:static lg:pt-20">
-                <x-button text="{{ __('login.button_login') }}" />
+                <a href="{{ route('login.index') }}">
+                    <x-button text="{{ __('login.button_login') }}" />
+                </a>
             </div>
         </div>
     </div>
