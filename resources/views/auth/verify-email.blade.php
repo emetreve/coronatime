fbc86c00ef2bd0b29b38db4e8f7a8c86e7421de2
emetreve <emetreve@gmail.com>
2023-04-13 17:52:53 +0400

    style: make verify-email page responsive

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
     <div class="min-h-screen px-4">
-        <img class="h-14 pt-6" src="{{ asset('images/logo.png') }}" />
-        <div class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
-            <img class="h-20 pt-6" src="{{ asset('checkmark.gif') }}" />
-            <p class="text-gray-600 mt-4">{{ __('email-verification.sent_email') }}</p>
+        <img class="h-14 pt-6 lg:h-20 lg:mx-auto" src="{{ asset('images/logo.png') }}" />
+        <div class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center pb-16 lg:pb-32">
+            <img class="h-20 pt-6 lg:h-24" src="{{ asset('checkmark.gif') }}" />
+            <p class="text-gray-600 mt-4 lg:text-2xl">{{ __('email-verification.sent_email') }}</p>
         </div>
     </div>
 </x-layout>
