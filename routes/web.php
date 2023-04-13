a4e91507f679ef40e3aeb663349ea94066b10f7a
emetreve <emetreve@gmail.com>
2023-04-13 14:41:36 +0400

    fix: view instead of get

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::view('/signup', 'auth.signup')->middleware('guest')->name('signup.index')
 Route::post('/signup', [AuthController::class, 'signup'])->middleware('guest')->name('signup');
 
 Route::view('/signup/success', 'auth.signup-success')->name('signup.success');
-Route::get('/email/verify', 'auth.verify-email')->middleware('auth')->name('verification.notice');
+Route::view('/email/verify', 'auth.verify-email')->middleware('auth')->name('verification.notice');
 Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::view('/dashboard', 'admin.show')->middleware('verified')->name('dashboard');
