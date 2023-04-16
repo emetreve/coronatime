06f7f63c30de030d169b435e6e98f3043ccac648
emetreve <emetreve@gmail.com>
2023-04-16 22:57:41 +0400

    wip: worldwide markup

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -1,9 +1,15 @@
 <x-layout>
     <div class="min-h-screen flex flex-col">
         <x-header />
-        <div class="flex-grow bg-worldwidebg">
+        <div class="flex-grow lg:bg-worldwidebg pl-4 lg:pl-32 lg:pt-5">
 
-            <h1 class="font-bold text-[1.2rem] pt-10 lg:pt-32 lg:text-[1.8rem]">Worldwide Statistics</h1>
+            <h1 class="font-extrabold py-8 text-lg lg:text-3xl">Worldwide Statistics</h1>
+
+            <div class="border-b border-gray-200 pb-2 lg:pb-4 lg:pt-3">
+                <p class="inline mr-5 font-bold border-b-[0.2rem] pb-2 border-black lg:pb-4 lg:text-lg lg:mr-14">
+                    Worldwide</p>
+                <p class="inline lg:text-lg">By country</p>
+            </div>
 
             {{-- <x-new-cases />
     <x-recovered />
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,16 @@
-<div class="relative lg:mx-28 pb-6">
-    <div class="flex items-center justify-between pt-6 px-4 w-full">
-        <img class="h-8 lg:h-10 inline" src="{{ asset('images/logo.png') }}" alt="logo" />
-        <div class="flex items-center">
-            <p class="inline">English</p>
-            <img class="ml-2 inline h-[0.4rem]" src="{{ asset('images/language-switcher.png') }}"
-                alt="language-switched" />
-            <img class="ml-8 h-[0.9rem] inline lg:hidden" src="{{ asset('images/burger-menu.png') }}" alt="logo" />
-            <p class="hidden lg:inline ml-14 font-semibold">Takeshi K.</p>
-            <p class="hidden lg:inline ml-8 pl-4 py-1 border-l border-gray-200"><a>Log Out</a></p>
+<div class="border-b border-gray-200">
+    <div class="relative lg:mx-28 pb-6">
+        <div class="flex items-center justify-between pt-6 px-4 w-full">
+            <img class="h-8 lg:h-12 inline" src="{{ asset('images/logo.png') }}" alt="logo" />
+            <div class="flex items-center">
+                <p class="inline lg:text-lg">English</p>
+                <img class="ml-2 inline h-[0.4rem] lg:h-[0.46rem]" src="{{ asset('images/language-switcher.png') }}"
+                    alt="language-switched" />
+                <img class="ml-8 h-[0.9rem] inline lg:hidden" src="{{ asset('images/burger-menu.png') }}"
+                    alt="logo" />
+                <p class="hidden lg:inline ml-14 font-semibold text-lg">Takeshi K.</p>
+                <p class="hidden lg:inline text-lg ml-8 pl-4 py-1 border-l border-gray-100"><a>Log Out</a></p>
+            </div>
         </div>
     </div>
 </div>
-<hr />
