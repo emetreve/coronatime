3b7a36113bd1a8e8ef9923112b31f2ecf8e58206
emetreve <emetreve@gmail.com>
2023-04-16 20:44:36 +0400

    wip: header markup

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,15 @@
-<div class="relative">
-    <div class="pt-2 px-4 w-full">
-        <img class="h-14 pt-6 lg:h-20" src="{{ asset('images/logo.png') }}" alt="logo" />
-        <div>
-            <p>English</p>
-            <img src="{{ asset('images/logo.png') }}" />
+<div class="relative lg:mx-32 pb-6">
+    <div class="flex items-center justify-between pt-6  px-4 w-full">
+        <img class="h-8 lg:h-10 inline" src="{{ asset('images/logo.png') }}" alt="logo" />
+        <div class="flex items-center">
+            <p class="inline">English</p>
+            <img class="ml-2 inline h-[0.4rem]" src="{{ asset('images/language-switcher.png') }}"
+                alt="language-switched" />
+            <img class="ml-8 mr-1 h-[0.9rem] inline lg:hidden" src="{{ asset('images/burger-menu.png') }}"
+                alt="logo" />
+            <p class="hidden lg:inline ml-14 font-semibold">Takeshi K.</p>
+            <p class="hidden lg:inline ml-8 pl-4 py-1 border-l border-gray-200"><a>Log Out</a></p>
         </div>
     </div>
 </div>
+<hr />
