814244d78c35beb13f7b4640c87037c2c8d32391
emetreve <emetreve@gmail.com>
2023-04-17 16:14:44 +0400

    wip: worldwide markup

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -11,17 +11,18 @@
                 <p class="inline lg:text-lg">By country</p>
             </div>
 
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
-                <div class="lg:col-span-1 col-span-3 bg-slate-600">
+            <div class="grid grid-cols-2 lg:grid-cols-3 pt-6 pr-4 lg:pr-28">
+                <div class="lg:col-span-1 col-span-3 bg-slate-600 h-48 mb-4 lg:mr-6 lg:h-80">
                     hello1
                 </div>
-                <div class="lg:col-span-1 col-span-1 bg-orange-200">
+                <div class="lg:col-span-1 col-span-1 bg-orange-200 h-52 mr-2 lg:mr-3 lg:h-80">
                     hello2
                 </div>
-                <div class="lg:col-span-1 col-span-1 bg-yellow-300">
+                <div class="lg:col-span-1 col-span-1 bg-yellow-300 h-52 ml-2 lg:mr-3 lg:h-80">
                     hello3
                 </div>
             </div>
+
         </div>
     </div>
 </x-layout>
