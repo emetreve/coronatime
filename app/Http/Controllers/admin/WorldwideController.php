bbee24ee8f1c2c6a328fc8ee80ddf203663baed8
emetreve <emetreve@gmail.com>
2023-04-18 22:37:48 +0400

    refactor: use model over DB::table

--- app/Http/Controllers/admin/WorldwideController.php
+++ app/Http/Controllers/admin/WorldwideController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Covidstat;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class WorldwideController extends Controller
@@ -16,9 +16,9 @@ class WorldwideController extends Controller
 		return view('admin.show', [
 			'userName'  => Auth::user()->name,
 			'language'  => Config::get('languages')[App::getLocale()],
-			'deaths'    => number_format(DB::table('covidstats')->sum('deaths')),
-			'recovered' => number_format(DB::table('covidstats')->sum('recovered')),
-			'newCases'  => number_format(DB::table('covidstats')->sum('confirmed')),
+			'deaths'    => number_format(Covidstat::sum('deaths')),
+			'recovered' => number_format(Covidstat::sum('recovered')),
+			'newCases'  => number_format(Covidstat::sum('confirmed')),
 		]);
 	}
 }
