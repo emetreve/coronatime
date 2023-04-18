7b9500324475c3561eab4c035128f0c862aeb134
emetreve <emetreve@gmail.com>
2023-04-18 20:50:34 +0400

    refactor: remove comment

--- app/Models/Covidstat.php
+++ app/Models/Covidstat.php
@@ -5,13 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// use Spatie\Translatable\HasTranslations;
-
 class Covidstat extends Model
 {
 	use HasFactory;
 
 	public $guarded = [];
-
-	// public $translatable = ['name'];
 }
