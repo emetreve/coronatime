be80a0ef26fe13e84d9d56655b5a961835caedb1
emetreve <emetreve@gmail.com>
2023-04-19 13:58:58 +0400

    feat: add username in burger menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -32,6 +32,10 @@
                     src="{{ asset('images/burger-menu.png') }}" alt="logo" />
                 <div class="px-5 absolute top-5 mt-8 bg-white rounded-md shadow-lg hidden right-1" id="burger-content">
                     <ul class="flex flex-col items-center">
+                        <li class="hover:bg-gray-100 pt-4 text-center w-full text-sm font-semibold lg:text-lg"
+                            onclick="document.querySelector('#logout').submit()">
+                            {{ $userName }}</p>
+                        </li>
                         <li class="hover:bg-gray-100 py-3 text-center w-full text-sm lg:text-lg"
                             onclick="document.querySelector('#logout').submit()">
                             {{ __('dashboard.log_out') }}</p>
