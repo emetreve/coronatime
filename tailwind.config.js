9ed1866ea9ad6fe55cefb8300921e8eb293cc311
emetreve <emetreve@gmail.com>
2023-04-20 11:43:23 +0400

    style: apply google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
         'green-opacity-8': 'rgba(36, 158, 44, 0.08)',
         'yellow-opacity-8': 'rgba(234, 214, 33, 0.08)'
       },
+      fontFamily: {
+        inter: ['Inter']
+      }
     },
   },
   plugins: [
