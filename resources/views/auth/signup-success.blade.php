1714cc17a0dc295e11635d8e699e30414fb83cca
emetreve <emetreve@gmail.com>
2023-04-21 16:28:21 +0400

    refactor: move gif inside images dir

--- resources/views/auth/signup-success.blade.php
+++ resources/views/auth/signup-success.blade.php
@@ -2,7 +2,7 @@
     <div class="min-h-screen px-4 relative">
         <img class="h-14 pt-6 lg:h-20 lg:mx-auto" src="{{ asset('images/logo.png') }}" alt="logo" />
         <div class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center pb-16 lg:pb-32">
-            <img class="h-20 pt-6 lg:h-24" src="{{ asset('checkmark.gif') }}" />
+            <img class="h-20 pt-6 lg:h-24" src="{{ asset('images/checkmark.gif') }}" />
             <p class="text-gray-600 text-center mt-4 lg:text-2xl">{{ __('email-verification.account_confirmed') }}</p>
             <div class="w-full px-4 absolute bottom-5 lg:w-1/4 lg:static lg:pt-20">
                 <a href="{{ route('login.index') }}">
