46c7da7de72ac6c98b1b89a87fb59425f8de5cf9
emetreve <emetreve@gmail.com>
2023-04-23 16:47:28 +0400

    test: finish language controller test

---
+++ tests/Feature/LanguageTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+
+class LanguageTest extends TestCase
+{
+	public function test_locale_middleware_changes_app_locale()
+	{
+		$appLocale = 'ka';
+
+		$response = $this->withSession(['applocale' => $appLocale])->get(route('login.index'));
+
+		$response->assertOk();
+		$this->assertEquals($appLocale, App::getLocale());
+	}
+
+	public function test_language_controller_can_set_language_and_redirect_back()
+	{
+		$lang = 'ka';
+
+		$response = $this->get(route('lang.switch', $lang));
+		$response->assertRedirect();
+		$response->assertSessionHas('applocale', $lang);
+	}
+}
