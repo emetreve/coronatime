f2e3bdfc543d8d9787cf23505b4b0656eff97ca8
emetreve <emetreve@gmail.com>
2023-04-23 16:03:47 +0400

    wip: AuthController tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,12 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Http\Middleware\VerifyCsrfToken;
 
 class AuthTest extends TestCase
 {
 	use RefreshDatabase;
+
+	private User $user;
+
+	protected function setUp(): Void
+	{
+		parent::setUp();
+		$this->user = User::factory()->create();
+	}
 
 	public function test_login_page_is_accessible(): void
 	{
@@ -16,7 +26,7 @@ class AuthTest extends TestCase
 		$response->assertViewIs('auth.index');
 	}
 
-	public function test_auth_should_give_errors_if_inputs_are_not_provided()
+	public function test_auth_should_give_errors_if_inputs_are_not_provided(): void
 	{
 		$response = $this->post(route('login'));
 		$response = $this->post(route('login'), [
@@ -30,7 +40,7 @@ class AuthTest extends TestCase
 		]);
 	}
 
-	public function test_auth_should_give_username_error_if_we_dont_give_username_input()
+	public function test_auth_should_give_username_error_if_we_dont_give_username_input(): void
 	{
 		$response = $this->post(route('login'));
 		$response = $this->post(route('login'), [
@@ -43,7 +53,7 @@ class AuthTest extends TestCase
 		]);
 	}
 
-	public function test_auth_should_give_password_error_if_we_dont_give_password_input()
+	public function test_auth_should_give_password_error_if_we_dont_give_password_input(): void
 	{
 		$response = $this->post(route('login'));
 		$response = $this->post(route('login'), [
@@ -56,7 +66,7 @@ class AuthTest extends TestCase
 		]);
 	}
 
-	public function test_auth_should_give_incorrect_credentials_error_if_such_user_does_not_exist()
+	public function test_auth_should_give_incorrect_credentials_error_if_such_user_does_not_exist(): void
 	{
 		$response = $this->post(route('login'));
 		$response = $this->post(route('login'), [
@@ -65,10 +75,33 @@ class AuthTest extends TestCase
 			'_token'   => csrf_token(),
 		]);
 
-		// $response->ddSession();
-
 		$response->assertSessionHasErrors([
 			'username',
 		]);
+	}
+
+	public function test_auth_should_redirect_do_dashboard_after_successful_login(): void
+	{
+		$response = $this->actingAs($this->user)->get(route('dashboard'));
+		$response->assertSuccessful();
+	}
+
+	public function test_user_can_successfully_log_out()
+	{
+		$response = $this->withoutMiddleware(VerifyCsrfToken::class)->actingAs($this->user)->post(route('logout'));
+		$response->assertRedirect(route('login.index'));
+	}
+
+	public function test_signup_should_create_user_and_redirect_to_verification_notice()
+	{
+		$request = [
+			'name'                 => 'user',
+			'email'                => 'user@redberry.ge',
+			'password'             => 'pass',
+			'password_confirmation'=> 'pass',
+			'_token'               => csrf_token(),
+		];
+		$response = $this->withoutMiddleware(VerifyCsrfToken::class)->post(route('signup'), $request);
+		$response->assertRedirect(route('verification.notice'));
 	}
 }
