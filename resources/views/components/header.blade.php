c555f3ce820bdd6f6adf6fe1c696fb0a18e30969
emetreve <emetreve@gmail.com>
2023-04-26 13:47:00 +0400

    fix: make whole language div clickable to load switcher

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,29 +4,30 @@
         <div class="flex items-center justify-between pt-6 px-4 w-full">
             <img class="h-8 lg:h-12 inline" src="{{ asset('images/logo.png') }}" alt="logo" />
             <div class="flex items-center">
+                <span id="dropdown-toggle" class="cursor-pointer flex items-center">
+                    <p class="inline lg:text-lg">
+                        {{ $language }}
+                    </p>
 
-                <p class="inline lg:text-lg">
-                    {{ $language }}
-                </p>
+                    <div class="relative">
+                        <img class="ml-2 inline h-[0.4rem] lg:h-[0.46rem]"
+                            src="{{ asset('images/language-switcher.png') }}">
+                        <div class="w-28 absolute top-0 mt-8 bg-white rounded-md shadow-lg hidden -right-4"
+                            id="dropdown-content">
+                            <ul class="flex flex-col items-center">
 
-                <div class="relative">
-                    <img class="ml-2 inline h-[0.4rem] lg:h-[0.46rem] cursor-pointer"
-                        src="{{ asset('images/language-switcher.png') }}" id="dropdown-toggle">
-                    <div class="w-28 absolute top-0 mt-8 bg-white rounded-md shadow-lg hidden right-1"
-                        id="dropdown-content">
-                        <ul class="flex flex-col items-center">
+                                <li class="hover:bg-gray-100 py-2 text-center w-full text-sm lg:text-lg"><a
+                                        href="{{ route('lang.switch', ['lang' => 'en']) }}">{{ __('dashboard.english') }}</a>
+                                </li>
 
-                            <li class="hover:bg-gray-100 py-2 text-center w-full text-sm lg:text-lg"><a
-                                    href="{{ route('lang.switch', ['lang' => 'en']) }}">{{ __('dashboard.english') }}</a>
-                            </li>
+                                <li class="hover:bg-gray-100 py-2 text-center w-full text-sm lg:text-lg"><a
+                                        href="{{ route('lang.switch', ['lang' => 'ka']) }}">{{ __('dashboard.georgian') }}</a>
+                                </li>
 
-                            <li class="hover:bg-gray-100 py-2 text-center w-full text-sm lg:text-lg"><a
-                                    href="{{ route('lang.switch', ['lang' => 'ka']) }}">{{ __('dashboard.georgian') }}</a>
-                            </li>
-
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                </span>
 
                 <img id="burger-toggle" class="ml-8 h-[0.9rem] inline hover:cursor-pointer lg:hidden"
                     src="{{ asset('images/burger-menu.png') }}" alt="logo" />
